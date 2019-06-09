8316f2dc4df07eb1f6209df66e7830c542df9f91
wangming <178963228@qq.com>
2019-06-10 01:16:00 +0800

    Initial commit

---
+++ src/main/java/com/qf/springbootgit/controller/OutController.java
@@ -0,0 +1,13 @@
+package com.qf.springbootgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wangming
+ * @Date 2019/6/10
+ */
+@Controller
+@RequestMapping("our")
+public class OutController {
+}
